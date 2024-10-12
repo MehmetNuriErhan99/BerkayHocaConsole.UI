5be248792243cd83e1ddd25f0e963df97372e29f
Mehmet Nuri Erhan <96691585+MehmetNuriErhan99@users.noreply.github.com>
2024-10-12 13:43:32 +0300

    Add project files.

---
+++ LinQ/Program.cs
@@ -0,0 +1,32 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+//// where 
+//int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+//var result = numbs.Where(x=> x%2 == 0);
+
+//foreach(var x in result)
+//{
+//    Console.WriteLine(x);
+//}
+
+
+//List<Student> students = new List<Student> {
+//    new Student {Isim ="berkay", Not =10 } ,
+//    new Student {Isim ="Melis", Not =90 } ,
+//};
+
+//var result1 = students.Where(x => x.Not>=60);
+
+//foreach(var x in result1)
+//{ Console.WriteLine(x.Isi); }
+//class Student
+//{
+//    public string Isim { get; set; }
+//    public int  Not { get; set; }
+//}
+
+List<object > mixList = new() { "berkay"}
